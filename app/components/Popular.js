9c820132cccac4d36846cb42de9b0a07106fc424
Mariana <mariana.barros@academiadecodigo.org>
2020-01-30 18:06:17 +0000

    add functional component and prop-types implementation

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -1,11 +1,46 @@
-import React from "react";
+import React from 'react';
+import PropTypes from 'prop-types';
+
+//Abstraction of list on its own statless functional component
+//it's still a pure function even though it's receiving a cb that changes the state
+//function LanguagesNav(props) {
+function LanguagesNav({ selected, onUpdateLanguage }) {
+  const languages = ['All', 'JavaScript', 'Ruby', 'Java', 'CSS', 'Python'];
+
+  return (
+      //it's returning a js object, all of this is js! See babel website :3
+    <ul className='flex-center'>
+      {languages.map(language => (
+        <li key={language}>
+          <button
+            className='btn-clear nav-link'
+            style={language === selected ? { color: 'rgb(187,46,31)' } : null}
+            onClick={() => {
+              onUpdateLanguage(language);
+            }}
+          >
+            {language}
+          </button>
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+LanguagesNav.propTypes = {
+    selected: PropTypes.string.isRequired,
+    onUpdateLanguage: PropTypes.func.isRequired
+}
+
 
 export default class Popular extends React.Component {
   constructor(props) {
     super(props);
+
     this.state = {
-      selectedLanguage: "All"
+      selectedLanguage: 'All'
     };
+
     this.updateLanguage = this.updateLanguage.bind(this);
   }
 
@@ -14,24 +49,16 @@ export default class Popular extends React.Component {
   }
 
   render() {
-    const languages = ["All", "JavaScript", "Ruby", "Java", "CSS", "Python"];
+    //destructuring - is going to select only the selectedLanguage property
+    const { selectedLanguage } = this.state;
 
     return (
-      <ul className="flex-center">
-        {languages.map(language => (
-          <li key={language}>
-            <button
-              className="btn-clear nav-link"
-              style = {(language === this.state.selectedLanguage ? { color: 'rgb(187,46,31)' } : null)}
-              onClick={() => {
-                this.updateLanguage(language);
-              }}
-            >
-              {language}
-            </button>
-          </li>
-        ))}
-      </ul>
+      <React.Fragment>
+        <LanguagesNav
+          selected={selectedLanguage}
+          onUpdateLanguage={this.updateLanguage}
+        ></LanguagesNav>
+      </React.Fragment>
     );
   }
 }
