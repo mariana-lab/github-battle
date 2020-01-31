6715e76e7410a15d6f1339b1146786c28264f212
Mariana <mariana.barros@academiadecodigo.org>
2020-01-31 16:53:48 +0000

    add repos grid

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -1,6 +1,12 @@
 import PropTypes from "prop-types";
 import React from "react";
 import { fetchPopularRepos } from "../utils/api";
+import {
+  FaUser,
+  FaStar,
+  FaCodeBranch,
+  FaExclamationTriangle
+} from "react-icons/fa";
 
 //Abstraction of list on its own statless functional component
 //it's still a pure function even though it's receiving a cb that changes the state
@@ -33,6 +39,59 @@ LanguagesNav.propTypes = {
   onUpdateLanguage: PropTypes.func.isRequired
 };
 
+function ReposGrid({ repos }) {
+  return (
+    <ul className="grid space-around">
+      {repos.map((repo, index) => {
+        const {
+          name,
+          owner,
+          html_url,
+          stargazers_count,
+          forks,
+          open_issues
+        } = repo;
+        const { login, avatar_url } = owner;
+        return (
+          <li key={html_url} className="repo bg-light">
+            <h4 class="header-lg center-text">#{index + 1}</h4>
+            <img
+              className="avatar"
+              src={avatar_url}
+              alt={`Avatar dor ${login}`}
+            />
+            <h2 className="center-text">
+              <a href={html_url}>{name}</a>
+            </h2>
+            <ul className="card-list">
+              <li>
+                <FaUser color="rgb(255,191,116)" size={22} />
+                <a href={`https://github.com/${login}`}>{login}</a>
+              </li>
+              <li>
+                <FaStar color='rgb(255, 215, 0)' size={22} />
+                {stargazers_count.toLocaleString()} stars
+              </li>
+              <li>
+                <FaCodeBranch color='rgb(129, 195, 245)' size={22} />
+                {forks.toLocaleString()} forks
+              </li>
+              <li>
+                <FaExclamationTriangle color='rgb(241, 138, 147)' size={22} />
+                {open_issues.toLocaleString()} open
+              </li>
+            </ul>
+          </li>
+        );
+      })}
+    </ul>
+  );
+}
+
+ReposGrid.propTypes = {
+  repos: PropTypes.array.isRequired
+};
+
 export default class Popular extends React.Component {
   constructor(props) {
     super(props);
@@ -56,12 +115,12 @@ export default class Popular extends React.Component {
 
     if (!this.state.repos[selectedLanguage]) {
       fetchPopularRepos(selectedLanguage)
-        .then((data) =>
-          this.setState(({repos}) => ({
-              repos:{
-                ...repos,
-                [selectedLanguage]: data
-              }
+        .then(data =>
+          this.setState(({ repos }) => ({
+            repos: {
+              ...repos,
+              [selectedLanguage]: data
+            }
           }))
         )
         .catch(() => {
@@ -75,7 +134,7 @@ export default class Popular extends React.Component {
   }
 
   isLoading() {
-      const { selectedLanguage, repos, error } = this.state;
+    const { selectedLanguage, repos, error } = this.state;
     return !repos[selectedLanguage] && error === null;
   }
 
@@ -91,8 +150,13 @@ export default class Popular extends React.Component {
         ></LanguagesNav>
         {this.isLoading() && <p>LOADING...</p>}
         {error && <p>{error}</p>}
-        {repos[selectedLanguage] && <pre>{JSON.stringify(repos[selectedLanguage], null, 2)}</pre>}
+
+        {repos[selectedLanguage] && (
+          <ReposGrid repos={repos[selectedLanguage]}></ReposGrid>
+        )}
       </React.Fragment>
     );
   }
 }
+
+//<pre>{JSON.stringify(repos[selectedLanguage], null, 2)}</pre>
